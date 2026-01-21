aefd289fc26b955d52949758af705aa73028ea8e
Subwoffer <jusdesliens@mac.com>
2026-01-21 11:59:33 +0100

    Update channel.js

--- api/channel.js
+++ api/channel.js
@@ -36,12 +36,11 @@ module.exports = async (req, res) => {
             });
         }
 
-        // Récupérer les infos des utilisateurs
         const fids = [...new Set(data.messages.map(m => m.data.fid))];
         const userInfos = await getUserInfos(fids, jwt);
 
         const casts = data.messages
-            .filter(msg => msg.data?.castAddBody?.text) // Filtrer les casts vides
+            .filter(msg => msg.data?.castAddBody?.text)
             .map(msg => {
                 const cast = msg.data.castAddBody;
                 const fid = msg.data.fid;
@@ -57,3 +56,85 @@ module.exports = async (req, res) => {
                 const trustRank = calculateTrustRank(ethosScore, likes, recasts);
 
                 return {
+                    hash: `0x${msg.hash}`,
+                    text: cast.text,
+                    author: {
+                        username: userInfo.username || `user${fid}`,
+                        displayName: userInfo.displayName || userInfo.username || `User ${fid}`,
+                        walletAddress: walletAddress.slice(0, 6) + '...' + walletAddress.slice(-4),
+                    },
+                    reactions: { likes, recasts },
+                    ethosScore,
+                    trustRank,
+                };
+            });
+
+        casts.sort((a, b) => b.trustRank - a.trustRank);
+
+        res.status(200).json({
+            success: true,
+            channel,
+            casts,
+        });
+
+    } catch (error) {
+        console.error('[API] Error:', error);
+        res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
+};
+
+async function getUserInfos(fids, jwt) {
+    const userInfos = {};
+    
+    try {
+        const batchSize = 100;
+        for (let i = 0; i < fids.length; i += batchSize) {
+            const batch = fids.slice(i, i + batchSize);
+            const fidsParam = batch.join(',');
+            
+            const response = await fetch(`https://hub.pinata.cloud/v1/userDataByFid?fid=${fidsParam}`, {
+                headers: {
+                    'accept': 'application/json',
+                    'authorization': `Bearer ${jwt}`
+                }
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                if (data.messages) {
+                    data.messages.forEach(msg => {
+                        const fid = msg.data.fid;
+                        const userDataBody = msg.data.userDataBody;
+                        
+                        if (!userInfos[fid]) {
+                            userInfos[fid] = {};
+                        }
+                        
+                        if (userDataBody.type === 'USER_DATA_TYPE_USERNAME') {
+                            userInfos[fid].username = userDataBody.value;
+                        } else if (userDataBody.type === 'USER_DATA_TYPE_DISPLAY') {
+                            userInfos[fid].displayName = userDataBody.value;
+                        }
+                    });
+                }
+            }
+        }
+    } catch (error) {
+        console.error('[API] Error fetching user infos:', error);
+    }
+    
+    return userInfos;
+}
+
+function getEthosScore(address) {
+    const hash = address.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
+    return 20 + (hash % 76);
+}
+
+function calculateTrustRank(ethosScore, likes, recasts) {
+    const social = Math.log(1 + likes + recasts);
+    return 0.75 * ethosScore + 0.25 * (social * 20);
+}
